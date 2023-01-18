e7ac93b75310414d937c7e6a3f476c2d5c505235
Nechai_Ihor <fredduke1983@gmail.com>
2023-01-19 00:43:06 +0200

    end 01 gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,53 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 const galleryRef = document.querySelector(".gallery");
 
-galleryItems.map((a) => {
-  const img = document.createElement("img");
-  img.src = `${a.preview}`;
-  img.classList.add("gallery__image");
-  img.style.cursor = "pointer";
-
-  galleryRef.append(img);
-});
-
-galleryRef.addEventListener("click", foo);
-
-function foo(e) {
-  console.log((e.target.src = "eee"));
+function createHTMLMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) =>
+      galleryRef.insertAdjacentHTML(
+        "beforeend",
+        `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+      )
+    )
+    .join("");
 }
+
+function modalImgScrn(imgSrc) {
+  basicLightbox
+    .create(`<img width="100%" height="100%" src="${imgSrc}">`)
+    .show();
+}
+
+function targetImg(e) {
+  e.preventDefault();
+  modalImgScrn(e.target.dataset.source);
+}
+
+createHTMLMarkup(galleryItems);
+galleryRef.addEventListener("click", targetImg);
+
+//!=============
+
+/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>; */
+
+//!=============
